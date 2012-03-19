5074cecbf2fb04aa3260fb571e6541f96f74c748
softhat <softhat@softhat-laptop.(none)>
2012-03-19 15:47:37 -0400

    changed to use the argparse

--- VistA/GetAllFileManSchemaLog.py
+++ VistA/GetAllFileManSchemaLog.py
@@ -25,6 +25,7 @@ from CreateConnection import createExpectConnection
 from eventqueue import IEvent, ThreadPool
 import FileManGlobalAttributes
 from time import sleep
+import argparse
 
 class GetFileManSchemaLogEvent(IEvent):
   def __init__(self, system, fileManFile, logDir):
@@ -42,24 +43,59 @@ class GetFileManSchemaLogEvent(IEvent):
   def __str__(self):
     return self.__expr__()
 
-def main():
-  allfilemanfile = open("c:/users/jason.li/git/expect-script/VistA/allfilemanfiles", "rb")
-  assert(allfilemanfile)
-  pool = ThreadPool(10)
-#  allfilemanfile = ["200","2100","9.4","165.5",".2", "2", "130","63","450",
-#                    "55","6925", "509850.9", "45", "604", "9002313.02",
-#                    "52","2260", "139.5", "115", ".11", ".31"]
-  system = 1
-  throttle = 20
+
+TEST_FILEMAN_FILE_LIST = ["200","2100","9.4","165.5",".2", "2", "130","63","450",
+                          "55","6925", "509850.9", "45", "604", "9002313.02",
+                          "52","2260", "139.5", "115", ".11", ".31"]
+
+DEFAULT_NUM_THREADS = 10
+DEFAULT_THROTTLE_NUMBER = 20
+DEFAULT_SLEEP_SECONDS = 1
+
+def runTaskInThreadPool(numTheads, fileManList, system, logDir):
+  threadPool = ThreadPool(numTheads)
   index = 0
-  for item in allfilemanfile:
+  for item in fileManList:
     item = item.strip()
     print ("Adding %s to the queue" % item)
-    pool.addEvent(GetFileManSchemaLogEvent(system, item, "c:/users/jason.li/git/expect-script/VistA/output/logs/"))
+    threadPool.addEvent(GetFileManSchemaLogEvent(system, item, logDir))
     index = index + 1
-    if (index % throttle) == 0:
-      print ("total file %d processed, sleeping for 1 seconds......" % index)
-      sleep(1)
-  pool.stop()
+    if (index % DEFAULT_THROTTLE_NUMBER) == 0:
+      print ("total file %d processed, sleeping for %d seconds......" % (index, DEFAULT_SLEEP_SECONDS))
+      sleep(DEFAULT_THROTTLE_NUMBER)
+  threadPool.stop()
+
+def main(inputFile, system, numberThreads, logDir):
+  print ("inputFile is %s" % inputFile)
+  allfilemanfile = open(inputFile, "rb")
+  assert(allfilemanfile)
+  runTaskInThreadPool(numberThreads, allfilemanfile, system, logDir)
+
+def testMain(system, numberThreads, logDir):
+  runTaskInThreadPool(numberThreads, TEST_FILEMAN_FILE_LIST, system, logDir)
+
 if __name__ == '__main__':
-  main()
+  parser = argparse.ArgumentParser(description='Get All FileMan File Schema')
+  parser.add_argument('-i', required=True, dest='inputFile',
+                      help='input file contains all fileman file# to retrieve the schema log')
+  parser.add_argument('-s', required=True, dest='system', choices='123',
+                      help='1: Cache on Windows, 2: Cache on Linux, 3: GTM on Linux')
+  parser.add_argument('-o', required=True, dest='outputDir',
+                      help='Output dirctory to store all the data dictionary file schema')
+  parser.add_argument('-n', required=False, dest="numOfThreads", type=int,
+                      default=DEFAULT_NUM_THREADS,
+                      help="number of threads to run in parallel")
+  parser.add_argument('-test', required=False, dest='isTest', default=False, action='store_true',
+                      help='is this the test run')
+  result = vars(parser.parse_args());
+  
+  print (result)
+  
+  system = int(result['system'])
+  numOfThreads = DEFAULT_NUM_THREADS
+  if result['numOfThreads']:
+    numOfThreads = result['numOfThreads']
+  if result['isTest'] == True:
+    testMain(system, numOfThreads, result['outputDir'])
+  else:
+    main(result['inputFile'], system, numOfThreads, result['outputDir'])
