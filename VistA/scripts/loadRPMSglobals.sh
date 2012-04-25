d26d5fcdc1500e5c2df0d94bed8a22b22e94eeb2
jason <jason@Ubuntu11.(none)>
2012-04-25 00:36:35 -0400

    change to have a git rep

--- VistA/scripts/loadRPMSglobals.sh
+++ VistA/scripts/loadRPMSglobals.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
+gitrep=$1
 rm -f mump.load
-for global in /opt/git/VistA-Variant/RPMS/Packages/*/Globals/*.zwr
+for global in ${gitrep}/Packages/*/Globals/*.zwr
 do
     echo $global >> mump.load
     mupip load -format=zwr \"${global}\" >> mump.load 2>&1
